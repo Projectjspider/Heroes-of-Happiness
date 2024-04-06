0351341ecad4505a46780cab07e5109be7af96f7
Projectjspider <165259072+Projectjspider@users.noreply.github.com>
2024-04-06 12:21:32 +0530

    Delete HeroesOfHappiness directory

--- HeroesOfHappiness/src/main/java/com/example/empmanegmentsystem/service/EmpService.java
+++
@@ -1,9 +0,0 @@
-package com.example.empmanegmentsystem.service;
-
-import com.example.empmanegmentsystem.entity.Employee;
-
-public interface EmpService {
-	public Employee loginEmp(String email,String password);
-	public String sendMailForResetPassword(String email);
-	public String resetPassword(String email,String password);
-}
--- HeroesOfHappiness/src/main/java/com/example/empmanegmentsystem/service/HrService.java
+++
@@ -1,9 +0,0 @@
-package com.example.empmanegmentsystem.service;
-
-import com.example.empmanegmentsystem.entity.Employee;
-
-public interface HrService {
-	public Employee loginHr(String email,String password);
-	public String sendMailForResetPassword(String email);
-	public String resetPassword(String email,String password);
-}
